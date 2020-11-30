d0620c56f5526f1c3913228948d7d9df431ac3b9
suryasiriki4 <cs18b035@iittp.ac.in>
2020-11-30 19:38:37 +0530

    resolved some errors in commentPage ui

--- src/components/QandA/Comments/CommentPage.js
+++ src/components/QandA/Comments/CommentPage.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react';
 import CommentList from './CommentList';
 
-<<<<<<< HEAD:src/components/QandA/CommentPage.js
 class CommentPage extends React.Component {
+
     constructor(props) {
+        
+        super(props);
+
         this.state = {
-            commentList: []
+            commentList: [],
         };
         this.updateComment = this.updateComment.bind(this);
     }
@@ -21,17 +24,6 @@ class CommentPage extends React.Component {
             this.state.commentList
         );
     }
-=======
-function CommentPage(props) {
-    const [commentList, setcommentList] = useState([])
-    const updateComment = (newComment) => {
-        setcommentList(CommentList.concat(newComment))
-    }
-    const link= props.link;
-    return (
-        <CommentList />
-    );
->>>>>>> b28f392e3753677430c2c8f682c3449ca38d0cd2:src/components/QandA/Comments/CommentPage.js
 }
 
 export default CommentPage;
